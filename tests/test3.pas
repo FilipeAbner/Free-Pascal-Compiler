b771ea71e5465853d13594bdab5192b93e633ad5
Filipe Abner <filipeabner18@gmail.com>
2025-05-15 19:55:35 -0300

    feat: add demonstration program with arithmetic and logical operations

---
+++ tests/test3.pas
@@ -0,0 +1,91 @@
+program Demonstracao;
+
+{$mode objfpc} // modo compatível com Pascal clássico
+
+type
+  TIntArray = array[1..5] of Integer; // Declaração de tipo de array
+
+var
+  a, b, c, d, e: Integer;  // Declaração de variáveis
+  arr: TIntArray;          // Declaração de vetor
+  i: Integer;
+  cond1, cond2: Boolean;
+
+{ Declaração de função }
+function Soma(x, y: Integer): Integer;
+begin
+  Soma := x + y; // Retorno de função + operador aritmético "+"
+end;
+
+function Subtracao(x, y: Integer): Integer;
+begin
+  Subtracao := x - y; // operador aritmético "-"
+end;
+
+function Multiplicacao(x, y: Integer): Integer;
+begin
+  Multiplicacao := x * y; // operador "*"
+end;
+
+function Divisao(x, y: Integer): Integer;
+begin
+  Divisao := x div y; // operador "/"
+end;
+
+function Resto(x, y: Integer): Integer;
+begin
+  Resto := x mod y; // operador "%"
+end;
+
+function Comparacoes(x, y: Integer): Boolean;
+begin
+  if (x < y) or (x <= y) or (x > y) or (x >= y) or (x = y) or (x <> y) then
+    Comparacoes := True // Operadores de comparação e operadores lógicos
+  else
+    Comparacoes := False;
+end;
+
+begin
+  // Atribuições
+  a := 10;
+  b := 5;
+  c := 2;
+
+  // Operadores aritméticos + parênteses
+  d := (a + b) * c - (a div b) + (a mod c);
+
+  // Chamada de funções como parte de expressão
+  e := Soma(a, b) + Subtracao(a, c) + Multiplicacao(b, c) + Divisao(a, b) + Resto(a, c);
+
+  // Controle de fluxo: if
+  if a > b then
+    writeln('a > b');
+
+  // Controle de fluxo: if-else
+  if c = 2 then
+    writeln('c é 2')
+  else
+    writeln('c não é 2');
+
+  // Controle de fluxo: while
+  i := 1;
+  while i <= 5 do
+  begin
+    arr[i] := i * 2;  // Atribuição e acesso a índice de vetor
+    i := i + 1;
+  end;
+
+  // Operadores lógicos
+  cond1 := True;
+  cond2 := False;
+  if (cond1 and not cond2) or (cond2 and cond1) then
+    writeln('Operadores lógicos avaliados.');
+
+  // Escopo com begin-end
+  begin
+    writeln('Dentro de escopo begin-end');
+  end;
+
+  // Acesso a vetor
+  writeln('Elemento arr[3] = ', arr[3]);
+end.
