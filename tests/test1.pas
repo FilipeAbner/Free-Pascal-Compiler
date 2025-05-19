36ba6f1bba55db7f5d93b7b621a770449d7cfbf1
FilipeAbner <filipeabner18@gmail.com>
2025-05-18 22:46:16 -0300

    test: simplify test1

--- tests/test1.pas
+++ tests/test1.pas
@@ -1,17 +1,21 @@
 program Teste;
-const
-  PI = 3.14;
-  Escape = #27;
+var
+  PI: double;
+
 var
   a, b: integer;
   msg: string;
 begin
-  msg := 'Hello ''World''! Valor de PI: ' + #10 + 'Fim.';
+  {
+    msg := 'Hello ''World''! Valor de PI: ' + #10 + 'Fim.';
+  }
   a := 10;
   b := $1F;
   a := b mod 3 + 2 * a - 1;
+  {
   if a > b then
     b := b + 1
   else
     b := b - 1;
+  }
 end.
